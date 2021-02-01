07ead6950ddb40d5126b43c4c912865e3c43547c
shin.mori <mori0510shin@gmail.com>
2021-02-01 13:44:34 +0900

    Add database files

---
+++ src/utils/DBUtil.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+ // データベース接続と結果取得のための変数
+    private static Connection con;
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        // 1. ドライバのクラスをJava上で読み込む
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2. DBと接続する
+        con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/drs_nohibernate?useSSL=false&useUnicode=true&characterEncoding=utf8",
+                "mori",
+                "morishita"
+                );
+
+        return con;
+    }
+
+    public static void close() {
+        // 7. 接続を閉じる
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
