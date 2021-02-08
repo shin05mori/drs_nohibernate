99462ab5c8faedff411d445f1d1613e5eda3926e
shin.mori <mori0510shin@gmail.com>
2021-02-08 16:31:03 +0900

    Add Report class

---
+++ src/models/Report.java
@@ -0,0 +1,81 @@
+package models;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Report {
+    private Integer id;
+    private Employee employee;
+    private Date report_date;
+    private String title;
+    private String content;
+    private Timestamp created_at;
+    private Timestamp updated_at;
+    private int count;
+
+    public Report() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+}
