590e2a561e18d0125d2a0ca0b35ab5bea1a68b19
shin.mori <mori0510shin@gmail.com>
2021-02-09 14:19:36 +0900

    Add Report edit

---
+++ src/controllers/reports/ReportsEditServlet.java
@@ -0,0 +1,48 @@
+package controllers.reports;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.SelectDAO;
+import models.Employee;
+import models.Report;
+
+/**
+ * Servlet implementation class ReportsEditServlet
+ */
+@WebServlet("/reports/edit")
+public class ReportsEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsEditServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SelectDAO dao = new SelectDAO();
+        Report r = dao.selectReportCode(Integer.parseInt(request.getParameter("id")));
+
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(r != null && login_employee.getId() == r.getEmployee().getId()) {
+            request.setAttribute("report", r);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("report_id", r.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
